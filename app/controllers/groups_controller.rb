46925499dc79a6f867f22fab6ef4e8e66553ac01
ouasamine <ouas.amine@outlook.com>
2023-02-14 16:28:09 +0100

    Create form to add new category

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,24 @@
 class GroupsController < ApplicationController
   def index
-
+    @groups = Group.where(user: current_user)
   end
 
   def new
+    @new_group = Group.new
+  end
 
+  def create
+    @new_group = Group.new(group_params)
+
+    if @new_group.save
+      flash[:success] = "Category created successfully."
+      redirect_to groups_path
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :user_id)
   end
 end
