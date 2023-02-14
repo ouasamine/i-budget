3b2cd5d8e0b064a735e63f2ad057cdebb7182196
ouasamine <ouas.amine@outlook.com>
2023-02-14 16:35:33 +0100

    Create total_amount method to return amount of group transactions

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,12 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :transacs
   validates :name, :user, presence: true
+
+  def total_amount
+    total = 0
+    transacs.each do |t|
+      total += t.amount
+    end
+    total
+  end
 end
