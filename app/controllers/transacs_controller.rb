603a2233cb1cd8e833f5c97fa2d22079e06eda59
ouasamine <ouas.amine@outlook.com>
2023-02-16 12:55:00 +0100

    Create form to add new transactions

--- app/controllers/transacs_controller.rb
+++ app/controllers/transacs_controller.rb
@@ -4,6 +4,22 @@ class TransacsController < ApplicationController
   end
 
   def new
+    @new_transac = Transac.new
+    @user_groups = Group.where(user: current_user)
+  end
 
+  def create
+    @new_transac = Transac.new(transac_params)
+
+    if @new_transac.save
+      flash[:success] = "Transaction created successfully."
+      redirect_to group_transacs_path(transac_params[:group_id])
+    end
+  end
+
+  private
+
+  def transac_params
+    params.require(:transac).permit(:name, :amount, :author_id, :group_id)
   end
 end
