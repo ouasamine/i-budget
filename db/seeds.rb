0b05d33a9b89d971b95a7ce04482e30b8910f4d8
ouasamine <ouas.amine@outlook.com>
2023-02-17 13:03:41 +0100

    Create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.create(name: "Tester", email: "test@example.com", password: 123456)
+group = Group.create(name: 'Test Group', user: user,
+                    icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Sign-check-icon.png/800px-Sign-check-icon.png')
+transaction = Transac.create(name: 'First transaction', amount: 155, author: user, group: group)
+transaction = Transac.create(name: 'Second transaction', amount: 245, author: user, group: group)
+transaction = Transac.create(name: 'Third transaction', amount: 345, author: user, group: group)
